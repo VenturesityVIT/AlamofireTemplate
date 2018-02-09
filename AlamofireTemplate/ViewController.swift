9cebe3a0f5f5fcc502c3c9f8a958dbe9e4b10764
s4rv4d <sarvadshetty@gmail.com>
2018-02-09 18:13:42 +0530

    finished get method

--- AlamofireTemplate/ViewController.swift
+++ AlamofireTemplate/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+struct network {
+    let url :String = "https://jsonplaceholder.typicode.com/"
+    let form = "posts"
+};
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var first: UILabel!
+    @IBOutlet weak var second: UILabel!
+    @IBOutlet weak var third: UILabel!
+    @IBOutlet weak var fourth: UILabel!
 
+    let net = network()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let finalURLString = net.url + net.form
+        getFunction(url: finalURLString)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Networking Methods
+   func getFunction(url:String){
+    Alamofire.request(url, method: .get).responseJSON { (response) in
+        if response.result.isFailure{
+            print(response.error!)
+        }
+        else if response.result.isSuccess{
+            let jsonData = JSON(response.value!)
+            print(jsonData)
+            self.updateLabels(data: jsonData)
+            
+        }
+    }
     }
 
-
+    //MARK: Update label using JSON Data
+    func updateLabels(data:JSON){
+        first.text = data[0]["title"].stringValue
+        second.text = data[0]["userId"].stringValue
+        third.text = data[0]["id"].stringValue
+        fourth.text = data[0]["body"].stringValue
+    }
 }
 
